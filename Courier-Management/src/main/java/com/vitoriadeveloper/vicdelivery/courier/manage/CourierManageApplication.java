829b1e4564a6703e21cd4145c1c18a44ed8ae4c1
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2025-11-03 21:12:45 -0300

    First Commit

---
+++ Courier-Management/src/main/java/com/vitoriadeveloper/vicdelivery/courier/manage/CourierManageApplication.java
@@ -0,0 +1,13 @@
+package com.vitoriadeveloper.vicdelivery.courier.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourierManageApplication.class, args);
+	}
+
+}
---
+++ Courier-Management/src/test/java/com/vitoriadeveloper/vicdelivery/courier/manage/CourierManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitoriadeveloper.vicdelivery.courier.manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
